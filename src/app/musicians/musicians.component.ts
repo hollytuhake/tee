fc0dcc97759a1fa023df990e971b99e1416969dc
Holly Tuhake <hollyjeanhansen@gmail.com>
2024-11-28 13:46:32 -0600

    Update musicians.component.ts
    grab just the musicians, undo duplicates etc.

--- src/app/musicians/musicians.component.ts
+++ src/app/musicians/musicians.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dates } from '../dates';
 
 @Component({
   selector: 'app-musicians',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusiciansComponent implements OnInit {
 
+  dates = dates;
+
+  justPerformers = performersToDisplay;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+const performerString = Array.from(Object.values(dates), performerString => performerString.performers)
+
+
+const newPerformerList: any[] = []
+
+for (var val of performerString){
+  var myArray: string[] = val.split(", ")
+  for(var val1 of myArray){
+    newPerformerList.push(val1)
+  }
+}
+
+const performerListNoBlanks: string[] = newPerformerList.filter(function (e) {
+  return e;
+})
+
+const removeDups = (performerListNoBlanks: string[]): string[] => {
+  return performerListNoBlanks.filter((item,
+      index) => performerListNoBlanks.indexOf(item) === index);
+}
+
+const performersToDisplay = removeDups(performerListNoBlanks)
